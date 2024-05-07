050210522a3816b7d9d82f307beee7be435704ae
Jack42chj <hojinch99@gmail.com>
2024-05-07 17:43:53 +0900

    update font family

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,6 @@ const router = createBrowserRouter([
 // 페이지 전체 공통 CSS
 const GlobalStyles = createGlobalStyle`
   ${reset};
-  @font-face {
-    font-family: 'Pretendard';
-    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff2")
-      format("woff2");
-    font-display: swap;
-  }
   * {
     box-sizing: border-box;
   }
