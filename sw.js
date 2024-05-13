82c15f51c0fd4989ed698a3f9d5127aa1a61232e
Jack42chj <hojinch99@gmail.com>
2024-05-13 16:01:08 +0900

    PWA Offline Page

--- sw.js
+++ sw.js
@@ -1,14 +1,48 @@
-// install event
-self.addEventListener("install", (e) => {
-    console.log("[Service Worker] installed");
+importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+const CACHE = "pwa-cache";
+
+const offlineFallbackPage = "offline.html";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
 
-// activate event
-self.addEventListener("activate", (e) => {
-    console.log("[Service Worker] actived", e);
+// install event
+self.addEventListener("install", async (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+    );
 });
+
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
 
 // fetch event
-self.addEventListener("fetch", (e) => {
-    console.log("[Service Worker] fetched resource " + e.request.url);
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    const preloadResp = await event.preloadResponse;
+
+                    if (preloadResp) {
+                        return preloadResp;
+                    }
+
+                    const networkResp = await fetch(event.request);
+                    return networkResp;
+                } catch (error) {
+                    const cache = await caches.open(CACHE);
+                    const cachedResp = await cache.match(offlineFallbackPage);
+                    return cachedResp;
+                }
+            })()
+        );
+    }
 });
